ab8ebeefd3052d130ed7f48d4e6bfb74f1349a7b
eliza_zhang <elizazhang@MacBook-Pro-495.local>
2026-02-12 22:35:04 -0500

    Refactor list page to use GalleryClient

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,36 +1,5 @@
-import { supabase } from '@/lib/supabaseClient'
+import GalleryClient from "@/app/components/GalleryClient"
 
-export default async function ListPage() {
-    const { data, error } = await supabase
-        .from('images')
-        .select('id, url, image_description')
-
-    if (error) {
-        return (
-            <div style={{ padding: '2rem' }}>
-                <h1>Error</h1>
-                <pre>{error.message}</pre>
-            </div>
-        )
-    }
-
-    return (
-        <div style={{ padding: '2rem' }}>
-            <h1>Images</h1>
-
-            <ul>
-                {data?.map((image) => (
-                    <li key={image.id} style={{ marginBottom: '1rem' }}>
-                        <div><strong>ID:</strong> {image.id}</div>
-                        <div><strong>Description:</strong> {image.image_description}</div>
-                        <img
-                            src={image.url}
-                            alt={image.image_description ?? 'image'}
-                            style={{ maxWidth: '300px', marginTop: '0.5rem' }}
-                        />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+export default function ListPage() {
+    return <GalleryClient showTopBar />
 }
